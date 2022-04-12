088201d97bf4c019733af09973150df2089405a5
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-12 20:05:29 +0530

    montesarrat font set

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
+import theme from "./theme";
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <App />
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['"Montserrat"', "Open Sans"].join(","),
+  },
+});
+
+export default theme;
