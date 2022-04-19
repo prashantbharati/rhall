2fd49af44acffc304f8246784bd0942178c52466
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-19 19:52:36 +0530

    opacity back whole page done

--- src/App.js
+++ src/App.js
@@ -10,11 +10,15 @@ const App = () => {
     <>
       <div
         style={{
-          backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)),url('${frontfirst}')`,
+          backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0.36) 0%, #0c0c0c 100%),url('${frontfirst}')`,
           width: "100%",
           height: "200vh",
           backgroundSize: "cover",
           backgroundPosition: "center",
+          backgroundColor: "transparent",
+          // background-image: ;
+          opacity: "1",
+          transition: "background 0.3s, border-radius 0.3s, opacity 0.3s",
         }}
       >
         <div style={{ height: "20%" }}>
