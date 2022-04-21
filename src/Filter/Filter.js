683252f693e14e3e5bb051039fd2e786301af37b
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-21 08:28:46 +0530

    img done but opacity needs changes

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import filterpage from "../images/filterpage.jpg";
 const Filter = () => {
-  return <div>Filter</div>;
+  return (
+    <>
+      <div
+        style={{
+          height: "80vh",
+          backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0.33) 0%, #A1A1A1 100%),url('${filterpage}')`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+    </>
+  );
 };
 
 export default Filter;
