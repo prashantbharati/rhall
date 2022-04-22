c63fd439d2f288da8cdf8ab78eff8eeee2aba14b
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-22 17:59:14 +0530

    about imgs done

--- src/aboutus/about.js
+++ src/aboutus/about.js
@@ -1,7 +1,18 @@
 import React from "react";
-
+import aboutpage from "../images/aboutpage.jpg";
 const about = () => {
-  return <div>about</div>;
+  return (
+    <>
+      <div
+        style={{
+          height: "80vh",
+          backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0.23) 0%, #A1A1A1 100%),url('${aboutpage}')`,
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    </>
+  );
 };
 
 export default about;
