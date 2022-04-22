6903764fb55a09fac65bd966f82c0727dbeb1eb6
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-04-22 14:11:20 +0530

    blog img done

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -1,6 +1,18 @@
 import React from "react";
+import blogpage from "../images/blogpage.jpg";
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <>
+      <div
+        style={{
+          height: "80vh",
+          backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0.18) 0%, #A1A1A1 100%),url('${blogpage}')`,
+          backgroundSize: "100% 100%",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></div>
+    </>
+  );
 };
 
 export default Blog;
