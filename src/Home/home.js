502e7d2bd9b732d6d14deb050c35f8e5cfab4abd
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-05-03 22:56:05 +0530

    map div major left done

--- src/Home/home.js
+++ src/Home/home.js
@@ -16,7 +16,16 @@ import {
   faPercent,
   faPercentage,
   faPlus,
+  faEnvelope,
+  faMapMarker,
+  faPhone,
 } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faFacebookF,
+  faLinkedin,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
   const classes = useStyles();
@@ -618,6 +627,230 @@ const Home = () => {
                 cost-effective solution for you. It ensures constant supply of
                 groundwater without having to depend on water tankers.
               </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+
+      {/* Map div begins */}
+
+      <div style={{ height: "100vh", backgroundColor: "white" }}>
+        <Grid container>
+          <Grid item md={1}></Grid>
+          <Grid item md={5}>
+            <br /> <br /> <br />
+            <Typography
+              style={{
+                color: "#494949",
+                fontSize: "40px",
+                fontWeight: "200",
+                lineHeight: "1em",
+                bottom: "1px",
+                padding: "20px",
+                borderWidth: "0 0 1px",
+                borderBlockEnd: "",
+                borderStyle: "solid",
+              }}
+            >
+              Buy online.
+            </Typography>
+            <Typography
+              style={{
+                color: "#494949",
+                fontSize: "40px",
+                fontWeight: "200",
+                lineHeight: "1em",
+                bottom: "1px",
+                padding: "20px",
+                borderWidth: "0 0 1px",
+                borderStyle: "solid",
+              }}
+            >
+              Get in touch.
+            </Typography>
+            <br />
+            <br />
+            <Typography
+              style={{
+                color: "#494949",
+                fontSize: "18px",
+                fontWeight: "200",
+                lineHeight: "1.5em",
+              }}
+            >
+              Headquartered in Hyderabad.
+            </Typography>
+            <Typography
+              style={{
+                color: "#494949",
+                fontSize: "18px",
+                fontWeight: "200",
+                lineHeight: "1.5em",
+              }}
+            >
+              Available pan-India.
+            </Typography>
+            <br />
+            <br />
+            <FontAwesomeIcon
+              style={{
+                height: "1em",
+                position: "relative",
+                display: "inline-block",
+                color: "#494949",
+              }}
+              icon={faMapMarker}
+            />
+            <Typography
+              style={{
+                color: "#494949",
+                paddingLeft: "30px",
+                position: "relative",
+                bottom: "17px",
+                fontWeight: "300",
+                fontSize: "0.8em",
+              }}
+            >
+              Hyderabad, Telangana, India
+            </Typography>
+            <FontAwesomeIcon
+              style={{
+                height: "1em",
+                position: "relative",
+                display: "inline-block",
+                color: "#494949",
+              }}
+              icon={faEnvelope}
+            />
+            <Typography
+              style={{
+                color: "#494949",
+                paddingLeft: "30px",
+                position: "relative",
+                bottom: "17px",
+                fontWeight: "300",
+                fontSize: "0.8em",
+              }}
+            >
+              info@rainmax.co.in
+            </Typography>
+            <FontAwesomeIcon
+              style={{
+                height: "1em",
+                position: "relative",
+                display: "inline-block",
+                color: "#494949",
+              }}
+              icon={faPhone}
+            />
+            <Typography
+              style={{
+                color: "#494949",
+                paddingLeft: "30px",
+                position: "relative",
+                bottom: "17px",
+                fontWeight: "300",
+                fontSize: "0.8em",
+              }}
+            >
+              091-89191-59431
+            </Typography>
+            <Typography
+              style={{
+                color: "#494949",
+                fontSize: "18px",
+                fontWeight: "200",
+                lineHeight: "1.5em",
+              }}
+            >
+              Our Social Pages
+            </Typography>
+            <br />
+            <div
+              style={{
+                height: "40px",
+                width: "40px",
+                display: "inline-block",
+              }}
+            >
+              <div
+                style={{
+                  height: "95%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "10%",
+                  backgroundColor: "white",
+                }}
+              >
+                <FontAwesomeIcon
+                  style={{
+                    height: "1.5em",
+                    position: "relative",
+                    display: "inline-block",
+                    color: "black",
+                  }}
+                  icon={faFacebookF}
+                />
+              </div>
+            </div>
+            &nbsp;
+            <div
+              style={{
+                height: "40px",
+                width: "40px",
+                display: "inline-block",
+              }}
+            >
+              <div
+                style={{
+                  height: "95%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "10%",
+                  backgroundColor: "white",
+                }}
+              >
+                <FontAwesomeIcon
+                  style={{
+                    height: "1.5em",
+                    position: "relative",
+                    display: "inline-block",
+                    color: "black",
+                  }}
+                  icon={faInstagram}
+                />
+              </div>
+            </div>
+            &nbsp;
+            <div
+              style={{
+                height: "40px",
+                width: "40px",
+                display: "inline-block",
+              }}
+            >
+              <div
+                style={{
+                  height: "95%",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "10%",
+                  backgroundColor: "white",
+                }}
+              >
+                <FontAwesomeIcon
+                  style={{
+                    height: "1.5em",
+                    position: "relative",
+                    display: "inline-block",
+                    color: "black",
+                  }}
+                  icon={faLinkedin}
+                />
+              </div>
             </div>
           </Grid>
         </Grid>
