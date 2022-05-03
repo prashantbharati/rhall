7e422cb215f7c5da90a839bb526fad04ec5b656d
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-05-03 15:34:17 +0530

    card part done

---
+++ src/aboutus/aboutuscard.js
@@ -0,0 +1,82 @@
+import React from "react";
+import sai from "../images/sai.png";
+import { Grid, Typography } from "@material-ui/core";
+const aboutuscard = () => {
+  return (
+    <div style={{ height: "100vh", width: "50%", backgroundColor: "#686868" }}>
+      <Grid container style={{ bottomMargin: "0" }}>
+        <Grid item md={1}></Grid>
+        <Grid item md={3}>
+          <img style={{ height: "50%" }} src={sai} alt="saiimage" />
+        </Grid>
+        <Grid item md={2}>
+          <Typography
+            style={{
+              marginBottom: "22px",
+              color: "#000000",
+              fontFamily: "Montserrat",
+              fontWeight: "800",
+              fontSize: "35px",
+            }}
+          >
+            Sai Prasad Renugunta
+          </Typography>
+
+          <Typography
+            style={{
+              color: "black",
+              fontFamily: "Montserrat",
+              //   fontSize: "15px",
+              fontWeight: "800",
+              textTransform: "uppercase",
+              lineHeight: "0.1em",
+            }}
+          >
+            FOUNDER
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container style={{ position: "relative", bottom: "50px" }}>
+        <Grid item md={1}></Grid>
+        <Grid item md={8}>
+          <Typography
+            style={{
+              textAlign: "justify",
+              color: "black",
+              fontFamily: "Montserrat",
+              fontSize: "18px",
+              fontWeight: "200",
+              lineHeight: "1.5em",
+              letterSpacing: "1px",
+            }}
+          >
+            Sustainable Architect | Green Entrepreneur A firm believer in the
+            vital need for eco-friendly technologies, Sai spends much of his
+            time developing and incorporating these into his decade-long
+            architecture and design practice. A chance visit to the township of
+            Auroville some years ago, led him to develop a keen interest in
+            mud-based construction, rainwater harvesting, and organic farming,
+            and pursue these concepts with entrepreneurial vigour. Sai affirms
+            that long drives and working out are his keys to stress management.
+            He is a vocal proponent of the superiority of Hyderabadi biryani and
+            Irani chai.
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default aboutuscard;
+
+/* 
+
+text-align: justify;
+    color: #686868;
+    font-family: "Montserrat", Sans-serif;
+    font-size: 18px;
+    font-weight: 200;
+    line-height: 1.5em;
+    letter-spacing: 1px;
+*/
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
 import theme from "./theme";
+
+import Aboutuscard from "./aboutus/aboutuscard.js";
 // import Card from "./blog/card.js";
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <App />
+    {/* <App /> */}
+    <Aboutuscard />
   </ThemeProvider>,
   document.getElementById("root")
 );
