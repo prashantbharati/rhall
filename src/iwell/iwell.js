ab2f252f9b761b0afb42445b9c6f3ed09011cd4b
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-05-09 11:19:59 +0530

    iw responsive

--- src/iwell/iwell.js
+++ src/iwell/iwell.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import iwell from "../images/iwellpage.jpg";
 import { Typography, Button, Grid } from "@material-ui/core";
 import frontfilter2 from "../images/frontfilter2.jpg";
 const Iwell = () => {
+  const [matches, setMatches] = useState(
+    window.matchMedia("(max-width: 450px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 450px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
+  const [matches2, setMatches2] = useState(
+    window.matchMedia("(max-width: 960px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 960px)")
+      .addEventListener("change", (e) => setMatches2(e.matches));
+  }, []);
+
   return (
     <>
       <div
@@ -13,25 +33,25 @@ const Iwell = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
-        <br />
-        <Typography
-          style={{
-            color: "#FFFFFF",
-            fontFamily: "Poppins",
-            fontSize: "90px",
-            fontWeight: "500",
-          }}
-          // variant="h1"
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Injection Well System
-        </Typography>
+        <div style={{ padding: "250px 0 250px  8%" }}>
+          <Typography
+            style={{
+              color: "#FFFFFF",
+              fontFamily: "Poppins",
+              fontSize: `${matches ? "70px" : "90px"}`,
+              fontWeight: "500",
+            }}
+            // variant="h1"
+          >
+            Injection Well System
+          </Typography>
+        </div>
       </div>
 
       <div style={{ boxSizing: "border-box", marginLeft: "40px" }}>
         <Grid container>
           {/* <Grid item sm={0} md={1}></Grid> */}
-          <Grid item sm={12} md={6}>
+          <Grid item xs={12} md={6}>
             <div
               style={{
                 height: "130vh",
@@ -68,30 +88,39 @@ const Iwell = () => {
                 building of 50 flats (10,000 sq. ft. rooftop area).
               </Typography>
               <br /> <br /> <br />
-              <Button
-                variant="outlined"
-                align="right"
+              <div
                 style={{
-                  width: "50%",
-                  fontSize: "16px",
-                  fontWeight: "600",
-                  textTransform: "uppercase",
-                  lineHeight: "1em",
-                  letterSpacing: "1.5px",
-                  textShadow: "0px 0px 0px rgb(0 0 0 / 30%)",
-                  fill: "#000000",
-                  color: "#000000",
-                  backgroundColor: "rgba(64,84,178,0)",
-                  borderStyle: "solid",
-                  borderWidth: "2px 2px 2px 2px",
-                  borderColor: "#1e1e1e",
-                  borderRadius: "5px 5px 5px 5px",
-                  boxShadow: "0px 0px 15px 0px rgb(0 0 0 / 11%)",
-                  padding: "15px 35px 15px 35px",
+                  display: "flex",
+                  flexDirection: "column",
+
+                  alignItems: `${!matches2 ? "" : "center"}`,
                 }}
               >
-                Download Broucher
-              </Button>
+                <Button
+                  variant="outlined"
+                  align="right"
+                  style={{
+                    width: "50%",
+                    fontSize: "16px",
+                    fontWeight: "600",
+                    textTransform: "uppercase",
+                    lineHeight: "1em",
+                    letterSpacing: "1.5px",
+                    textShadow: "0px 0px 0px rgb(0 0 0 / 30%)",
+                    fill: "#000000",
+                    color: "#000000",
+                    backgroundColor: "rgba(64,84,178,0)",
+                    borderStyle: "solid",
+                    borderWidth: "2px 2px 2px 2px",
+                    borderColor: "#1e1e1e",
+                    borderRadius: "5px 5px 5px 5px",
+                    boxShadow: "0px 0px 15px 0px rgb(0 0 0 / 11%)",
+                    padding: "15px 35px 15px 35px",
+                  }}
+                >
+                  Download Broucher
+                </Button>
+              </div>
             </div>
           </Grid>
 
