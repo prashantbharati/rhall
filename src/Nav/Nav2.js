9fbfda410cf230ea2e731dd71be09af5a2945225
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-05-11 13:45:36 +0530

    nav2 animations done

--- src/Nav/Nav2.js
+++ src/Nav/Nav2.js
@@ -3,8 +3,69 @@ import navicon from "../images/navicon1.png";
 import rainmaxlogo1 from "../images/rainmaxlogo1.png";
 import useStyles from "./styles.js";
 import close from "../images/close.png";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles2 = makeStyles((theme) => ({
+  animatedItem: {
+    animation: `$myEffect 1000ms ${theme.transitions.easing.easeInOut}`,
+  },
+  animatedItemExiting: {
+    animation: `$myEffectExit 1000ms ${theme.transitions.easing.easeInOut}`,
+  },
+  animatedItemExitingRight: {
+    animation: `$myEffectExitright 1000ms ${theme.transitions.easing.easeInOut}`,
+  },
+  animatedItem3: {
+    animation: `$myEffectExit3 1000ms ${theme.transitions.easing.easeInOut}`,
+  },
+
+  "@keyframes myEffect": {
+    "0%": {
+      opacity: 0,
+      transform: "translateX(-200%)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateX(0)",
+    },
+  },
+  "@keyframes myEffectExit": {
+    "0%": {
+      opacity: 1,
+      transform: "translateX(0)",
+    },
+    "100%": {
+      opacity: 0,
+      transform: "translateX(-200%)",
+    },
+  },
+
+  "@keyframes myEffectExitright": {
+    "0%": {
+      opacity: 0,
+      transform: "translateX(200%)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0)",
+    },
+  },
+
+  "@keyframes myEffectExit3": {
+    "0%": {
+      opacity: 0,
+      transform: "translateY(-200%)",
+    },
+    "100%": {
+      opacity: 1,
+      transform: "translateY(0)",
+    },
+  },
+}));
+
 const Nav2 = () => {
   const classes = useStyles();
+  const classes2 = useStyles2();
   const [matches, setMatches] = useState(
     window.matchMedia("(max-width: 500px)").matches
   );
@@ -27,10 +88,15 @@ const Nav2 = () => {
 
   const doit = () => {
     document.getElementById("nav2").style.display = "flex";
+    document.getElementById("nav2").classList.toggle(classes2.animatedItem);
+    document.getElementById("nav2").style.transform = "translateX(0%)";
   };
 
   const doit2 = () => {
-    document.getElementById("nav2").style.display = "none";
+    document
+      .getElementById("nav2")
+      .classList.toggle(classes2.animatedItemExiting);
+    document.getElementById("nav2").style.transform = "translateX(-200%)";
   };
 
   return (
